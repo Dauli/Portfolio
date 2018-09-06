38d9738ebc735c67b5d6c12a6dba34ae621483d6
Dauli Pamale Alexis Ange <alexisangedauli@gmail.com>
2018-09-06 22:48:10 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+$(function(){
+	// Slide show of the page
+	$.fx.interval = 0;
+	(function cycleBgImage(elem, bgimg){
+		elem.css('backgroundImage', bgimg)
+		.fadeTo(2000, 1, 'linear', function(){
+			$(this).delay(2000, 'fx')
+			.fadeTo(2000, 0, 'linear', function(){
+				var img = $(this).css('backgroundImage').split(','),
+				bgimg = img.concat(img[0]).splice(1).join(',');
+				cycleBgImage(elem, bgimg);
+			});
+		});
+	}($('#slides')));
+});
