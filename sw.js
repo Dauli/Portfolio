cd41eca75f71b426e5e355a393138d43963074d8
Dauli Pamale Alexis Ange <alexisangedauli@gmail.com>
2019-03-10 20:40:59 +0200

    Update sw.js
    sw version2

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-// empty file for now
+self.addEventListener('install', function(event) {
+  console.log('SW installed at ', new Date().toLocaleDateString());
+});
+
+self.addEventListener('active', function(event) {
+  console.log('SW activated at ', new Date().toLocaleDateString());
+});
