78f9dbf3946223a64a60e4c55fe6d83976b5ad62
Dauli Pamale Alexis Ange <alexisangedauli@gmail.com>
2019-03-10 20:20:33 +0200

    Update main.js
    ServiceWorker registration

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+// registring service worker
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('sw.js')
+	.then(console.log)
+	.catch(console.error)
+}
+
+
 $(function(){
 	// Slide show of the page
 	$.fx.interval = 0;
