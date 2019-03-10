b302a6fcc04a1a1310b8cbce6771b6425daab931
Dauli Pamale Alexis Ange <alexisangedauli@gmail.com>
2019-03-10 22:18:08 +0200

    Update sw.js
    offline v3

--- sw.js
+++ sw.js
@@ -12,7 +12,8 @@ self.addEventListener('active', function(event) {
 self.addEventListener('fetch', function (event) {
   // handle ofline functionality
   if (!navigator.onLine) {
-    event.respondWith(new Response('<h1> offline:( </h1>', {headers: {'Content-Type' : 'text/html'}} ) );
+    event.respondWith(new Response('<div style="backgroundColor:lightGray"> <h1>Offline:( </h1><p>Please check your internet connection and come back later!</p></div>', 
+    {headers: {'Content-Type' : 'text/html'}} ) );
   } else {
     console.log(event.request.url);
     event.respondWith(fetch(event.request));
