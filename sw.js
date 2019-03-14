b4268900077009dcca6de4f8c891311bcf644f41
Dauli Pamale Alexis Ange <alexisangedauli@gmail.com>
2019-03-14 22:39:21 +0200

    Update sw.js
    caches-1

--- sw.js
+++ sw.js
@@ -1,21 +1,13 @@
-const version = 2;
+const version = 1;
 
 self.addEventListener('install', function(event) {
-  console.log('SW v%s installed at ', version, new Date().toLocaleDateString());
-  self.skipWaiting();
-});
-
-self.addEventListener('active', function(event) {
-  console.log('SW v%s activated at ', version, new Date().toLocaleDateString());
-});
-
-self.addEventListener('fetch', function (event) {
-  // handle ofline functionality
-  if (!navigator.onLine) {
-    event.respondWith(new Response('<div style="backgroundColor:lightGray"> <h1>Offline:( </h1><p>Please check your internet connection and come back later!</p></div>', 
-    {headers: {'Content-Type' : 'text/html'}} ) );
-  } else {
-    console.log(event.request.url);
-    event.respondWith(fetch(event.request));
-  } 
+    event.waitUntil(
+        caches.open(version)
+        .then(function(cache) {
+            return cache.addAll([
+                '/offline/offline-embede.html',
+                'offline/ohno.png'
+            ]);
+        })
+    )
 });
