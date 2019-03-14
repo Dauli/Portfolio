205c7be461950a12bffd6eca53702b0da963473b
Dauli Pamale Alexis Ange <alexisangedauli@gmail.com>
2019-03-14 23:13:49 +0200

    Update sw.js
    cache-3

--- sw.js
+++ sw.js
@@ -5,8 +5,8 @@ self.addEventListener('install', function(event) {
         caches.open(version)
         .then(function(cache) {
             return cache.addAll([
-                '/offline/offline-embede.html',
-                'offline/ohno.png'
+                './offline-embede.html',
+                './ohno.png'
             ]);
         })
     )
