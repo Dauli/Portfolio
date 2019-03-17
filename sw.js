ca6a71a5dbc3bcb046356506f5a0cbf8f35750ef
Dauli Pamale Alexis Ange <alexisangedauli@gmail.com>
2019-03-17 14:55:37 +0200

    Update sw.js
    adding files link to sw

--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
-const version = 2;
+const version = 'v1';
 
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(version)
         .then(function(cache) {
             return cache.addAll([
-                '/content/offline-embeded.html'
+                '/content/offline-embeded.html',
+                'content/ohno.png'
             ]);
         })
     )
