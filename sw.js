960ba5bad7614bda962e005bf72fe72f37d0fadd
Dauli Pamale Alexis Ange <alexisangedauli@gmail.com>
2019-03-17 15:57:40 +0200

    Update sw.js
    adding caches impementation

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
+/*
+    This code containt the service Worker implementation coded by Alexis Ange Dauli
+    We install service worker to save caches that can be used offline 
+*/
+
 const version = 'v1';
 
+// lets add ressources that we want to cache
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(version)
@@ -8,6 +14,24 @@ self.addEventListener('install', function(event) {
                 'content/offline-embeded.html',
                 'content/ohno.png'
             ]);
+        })
+    )
+});
+
+// uses of caches that has been saved in browser
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(res) {
+            if(res) {
+                return res;
+            }
+            
+            if(!navigator.onLine) {
+                return caches.match(new Request('content/offline-embeded.html'));
+            }
+            
+            return fetch(event.request);
         })
     )
 });
