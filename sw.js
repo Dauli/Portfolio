578a4147893994d28000c300a8c4b4672c30ebef
Alexis Ange <alexisangedauli@gmail.com>
2019-04-02 23:01:03 +0200

    adding gallery and serviceWorker implementation

--- sw.js
+++ sw.js
@@ -3,7 +3,12 @@
     We install service worker to save caches that can be used offline 
 */
 
-const version = 'v1';
+/*
+** cache APIs help us to add content and to remove it whenever 
+** we want to
+*/
+
+const version = 'v2';
 
 // lets add ressources that we want to cache
 self.addEventListener('install', function(event) {
@@ -11,6 +16,7 @@ self.addEventListener('install', function(event) {
         caches.open(version)
         .then(function(cache) {
             return cache.addAll([
+                // we can add more ressources here as available
                 'content/offline-embeded.html',
                 'content/ohno.png'
             ]);
@@ -18,20 +24,56 @@ self.addEventListener('install', function(event) {
     )
 });
 
-// uses of caches that has been saved in browser
+// delete the old caches
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys()
+        .then(function(keys) {
+            return Promise.all(keys.filter(function(key) {
+                return key !== version;
+            }).map( function (key) {
+                return caches.delete(key);
+            }));
+        })
+    );
+});
+
+// lets use our cached ressources
 self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request)
         .then(function(res) {
-            if(res) {
+            if (res) {
                 return res;
             }
-            
-            if(!navigator.onLine) {
+
+            if (!navigator.onLine) {
                 return caches.match(new Request('content/offline-embeded.html'));
             }
-            
-            return fetch(event.request);
+
+            return fetchAndUpdate(event.request);
         })
     )
 });
+
+/*  
+** let user add content to the SW via caches 
+** so any link that user visit will be stores in cache to use in offline
+** but when user try to access a link that wasn't navigate before in online
+** the standard offline content will be dispalyed.
+*/
+function fetchAndUpdate(request) {
+    return fetch(request)
+    .then( function(res) {
+        if (res) {
+            return caches.open(version)
+            .then(function(cache) {
+                return cache.push(request, res.clone())
+                .then( function() {
+                    return res;
+                });
+            });
+        }
+    });
+}
+
